b6ef04bb06b1ed446ead8606a4857b33de534948
DESKTOP-3I3N22C\JesusPatricio <csi22-jpatano@colegioaltair.net>
2022-12-05 14:36:28 +0100

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireModule } from '@angular/fire/compat';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
+import { ListaMascotasComponent } from './vistas/lista-mascotas/lista-mascotas.component';
+import { DetalleMascotasComponent } from './vistas/detalle-mascotas/detalle-mascotas.component';
+import { FormularioMascotasComponent } from './vistas/formulario-mascotas/formulario-mascotas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaMascotasComponent,
+    DetalleMascotasComponent,
+    FormularioMascotasComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
